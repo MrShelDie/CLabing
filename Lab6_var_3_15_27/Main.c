83538aaa98a46754f6a58b4ab77435f35240e6fb
Nikolay <65782143+MrShelDie@users.noreply.github.com>
2021-04-26 13:43:28 +0300

    bag fix

--- Lab6_var_3_15_27/Main.c
+++ Lab6_var_3_15_27/Main.c
@@ -4,50 +4,60 @@
 #define EXIT_CODE 5
 #define BUFF_SIZE 64
 
+void DialogWithUser(Passenger** passengers, int* count_of_passengers);
+
+
 int main()
 {
     int count_of_passengers = 0;
 
     PrintTaskTopic();
+
     PromptUserInputPassengersCount(&count_of_passengers);
+
     Passenger** passengers = NewPassengersArray(count_of_passengers);
+
     FillPassengersArray(passengers, count_of_passengers);
 
-    PrintMenu();
-    int user_input;
-
-    while ((user_input = PromptUserInputNumber("Enter function number\n")) != EXIT_CODE)  // is not safe
-    {
-        switch (user_input)
-        {
-        case 1:
-            AppendInPassengersArray(FillPassengerData(NewPassenger()), &passengers, &count_of_passengers);
-            break;
-        case 2:
-            PrintPassengersArray(passengers, count_of_passengers);
-            break;
-        case 3:
-            PrintAllAirlineFlights(PromptUserInputStr("Enter the name of the airline\n"), passengers, count_of_passengers);
-            break;
-        case 4:
-        {
-            Passenger** passengers_on_the_same_flight = NewPassengersArray(count_of_passengers);
-            FindPassengersOnTheSameFlight(PromptUserInputNumber("Enter the flight number\n"), passengers, passengers_on_the_same_flight, count_of_passengers);
-            int count_of_passengers_on_the_same_flight = 0;
-            FitArraySize_Passenger(&passengers_on_the_same_flight, count_of_passengers, &count_of_passengers_on_the_same_flight);
-            SortPassengerArrayByName(passengers_on_the_same_flight, count_of_passengers_on_the_same_flight);
-            PrintPassengersArray(passengers_on_the_same_flight, count_of_passengers_on_the_same_flight);
-            FreePassengerArray(passengers_on_the_same_flight, count_of_passengers_on_the_same_flight);
-            break;
-        }
-        default:
-            break;
-        }
-
-        PrintMenu();
-    }
-
+	DialogWithUser(passengers, &count_of_passengers);
+   
     FreePassengerArray(passengers, count_of_passengers);
 
     return 0;
+}
+
+
+void DialogWithUser(Passenger** passengers, int* count_of_passengers)
+{
+	PrintMenu();
+	int user_input;
+
+	while ((user_input = PromptUserInputNumber("Enter function number\n")) != EXIT_CODE)  // is not safe
+	{
+		switch (user_input)
+		{
+		case 1:
+			AppendInPassengersArray(FillPassengerData(NewPassenger()), &passengers, count_of_passengers);
+			break;
+		case 2:
+			PrintPassengersArray(passengers, *count_of_passengers);
+			break;
+		case 3:
+		{
+			char* airline_name = NULL;
+			PrintAllAirlineFlights(PromptUserInputStr("Enter the name of the airline\n", airline_name), passengers, *count_of_passengers);
+			free(airline_name);
+			break;
+		}
+		case 4:
+		{
+			PrintPassengersOfGivenFlingt(passengers, *count_of_passengers);
+			break;
+		}
+		default:
+			break;
+		}
+
+		PrintMenu();
+	}
 }
